af12427449b4f162a5b72f7dfd8bee7a95adb2b1
PedroFabriz2 <44576768+PedroFabriz2@users.noreply.github.com>
2019-07-28 16:43:59 -0300

    Add files via upload

---
+++ JogoConsole.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <iostream>
+#include<vector>
+#include<fstream>
+#include<fstream>
+
+
+#include "JogoConsole.h"
+
+JogoConsole::JogoConsole():Jogo() {}
+
+JogoConsole::JogoConsole(string _nome, float _preco, string _genero,bool mult):Jogo(_nome, _preco, _genero){
+
+    setpossuiMultiplayer(mult);
+    qtdJogos++;
+    setID(qtdJogos);
+}
+
+bool JogoConsole::getpossuiMultiplayer()
+{
+    return this->possuiMultiplayer;
+}
+
+void JogoConsole::setpossuiMultiplayer(bool mul)
+{
+    this->possuiMultiplayer = mul;
+}
+
+void JogoConsole::imprimeInfoJogo()
+{
+    cout <<"ID característico do JOGO: "<< this->getID()<<endl;
+    cout << "JOGO CONSOLE"<<endl;// tipo e genero
+    cout << this->nome << endl;
+    cout << this->Jogo::getGenero()->getNome() << endl;
+    cout << this->getValorPromocao() << endl;
+    if(this->getpossuiMultiplayer() == true){
+        cout<< "possui multiplayer"<<endl;
+
+    }else{
+        cout<< "não possui multiplayer"<<endl;
+    }
+}
+
+float JogoConsole::getValorPromocao()
+{
+    if (emPromocao == 1)
+    {
+        return this->preco*0.9;
+    }
+    else
+    {
+        return this->preco;
+    }
+}
+
+void JogoConsole::salvarInfoJogo(ofstream &outfile){
+
+
+
+    outfile << "JOGOCONSOLE"<<endl;
+    outfile << this->nome <<endl;
+    outfile << this->getValorPromocao() <<endl;
+    outfile << this->getGenero()->getNome() <<endl;
+    outfile << this->getpossuiMultiplayer() <<endl;
+
+
+
+}
---
+++ JogoConsole.h
@@ -0,0 +1,40 @@
+#ifndef _JOGOCONSOLE_H
+#define _JOGOCONSOLE_H
+
+
+#include<vector>
+#include <string>
+#include <iostream>
+
+#include "JOGO.H"
+
+using namespace std;
+
+class JogoConsole: public Jogo
+{
+private:
+    bool possuiMultiplayer;
+
+public:
+    JogoConsole();
+
+    JogoConsole(string _nome, float _preco, string _genero,bool mult);
+
+    friend void presenteVirtual (PlataformaDigital *presenteado,PlataformaDigital *amigo, Jogo *presente);
+
+    bool getpossuiMultiplayer();
+
+    void setpossuiMultiplayer(bool mul);
+
+    void imprimeInfoJogo();
+
+    float getValorPromocao();
+
+    void salvarInfoJogo(ofstream &outfile);
+
+};
+
+
+
+
+#endif // _JOGOCONSOLE_H
